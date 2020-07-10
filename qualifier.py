fb73e373516784e4f747aa74e0f3a7347d58389e
Remy Bos <remy90@hotmail.com>
2020-07-10 12:15:45 +0200

    ARTICLE: Add minimum requirements functionality

--- qualifier.py
+++ qualifier.py
@@ -15,6 +15,9 @@ Important notes for submission:
 """
 import datetime
 import typing
+import string
+
+from collections import OrderedDict
 
 
 class ArticleField:
@@ -28,4 +31,77 @@ class Article:
     """The `Article` class you need to write for the qualifier."""
 
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
-        pass
+        self.title = title
+        self.author = author
+        self.publication_date = publication_date
+        self.content = content
+
+    def short_introduction(self, n_characters: int) -> str:
+        if self.__len__() <= n_characters:
+            introduction = self.content
+        else:
+            next_character = self.content[n_characters]
+            
+            if next_character in string.ascii_letters:
+                introduction = self.truncate_content(self.content[:n_characters])
+            else:
+                introduction = self.content[:n_characters]
+
+        return introduction
+
+    def truncate_content(self, content_up_to_n: str) -> str:
+        while content_up_to_n[-1] not in ' \n':
+          content_up_to_n = content_up_to_n[:-1]
+        
+        return content_up_to_n[:-1]
+
+    def most_common_words(self, n_words: int) -> dict:        
+        result = OrderedDict()
+        
+        for word in self.get_words():
+            if word not in result:
+                result[word] = 1
+            else:
+                result[word] += 1
+
+        return self.top(n_words, result)
+
+    def get_words(self) -> list:
+        content = self.remove_redundant_apostrophes(self.content)
+        content = self.remove_punctuation(content.lower())
+        content = content.replace('\n', ' ')
+        content = content.replace('\'', ' ')
+        
+        return content.split(' ')
+
+    def remove_redundant_apostrophes(self, content: str) -> str:
+        # Remove leading and trailing apostrophes
+        content = self.content.strip('\'')
+        # Remove trailing apostrophes followed by a new line
+        content = content.replace('\'\n', ' ')
+        # Remove quotes from words or sub texts
+        # something 'word' something :-> something word' something
+        content = content.replace(' \'', ' ')
+        # something word' something :-> something word something
+        content = content.replace('\' ', ' ')
+
+        return content
+
+    def remove_punctuation(self, content: str) -> str:
+        punctuation_characters = string.punctuation.replace('\'', '')
+        
+        for punctuation_character in punctuation_characters:
+            content = content.replace(punctuation_character, '')
+
+        return content
+
+    def top(self, n_words: int, word_count: dict) -> dict:
+        return dict(sorted(word_count.items(), key=lambda x: x[1], reverse=True)[:n_words])
+
+
+    def __repr__(self) -> str:
+        return f'<Article title={repr(self.title)} author={repr(self.author)} publication_date={repr(self.publication_date.isoformat())}>'
+
+    def __len__(self) -> int:
+        return len(self.content)
+
